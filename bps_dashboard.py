70ae52dcd667067ce1807407dca29493d73089d5
Bhagyabantapur-School <bhagyabantapurprimaryschool@gmail.com>
2026-05-02 23:37:51 +0530

    Update bps_dashboard.py

--- bps_dashboard.py
+++ bps_dashboard.py
@@ -50,7 +50,7 @@ def log_and_open(app_name, target_file):
             sheet.append_row([app_name, now_str])
     except Exception as e:
         print(f"Failed to log time: {e}")
-    cached_data = get_tracker_data() 
+    cached_data = get_bps_tracker_data() 
     cached_data[app_name] = now_str
     
     st.switch_page(target_file)
